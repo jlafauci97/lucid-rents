cbcf3108a4fc0414a3d877081dfb98cf764bc20b
Jesse Lafauci <256631659+jlafauci97@users.noreply.github.com>
2026-04-19 23:08:21 -0400

    fix(mission-control): hide sidebar on login via conditional shell

--- src/app/mission-control/layout.tsx
+++ src/app/mission-control/layout.tsx
@@ -1,10 +1,5 @@
-import { MCSidebar } from "@/components/mission-control/MCSidebar";
+import { MCLayoutShell } from "@/components/mission-control/MCLayoutShell";
 
 export default function MissionControlLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen bg-[#050B14] text-slate-100">
-      <MCSidebar />
-      <div className="flex-1 flex flex-col">{children}</div>
-    </div>
-  );
+  return <MCLayoutShell>{children}</MCLayoutShell>;
 }
